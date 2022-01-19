1603248dfe5bffb92916bda02325c3468cd4ec06
Artem Torlak <artem-972007@mail.ru>
2022-01-19 12:56:15 +0300

    [10] Create classes for each price & added method to Movie class to get price class by price_code

--- movie.rb
+++ movie.rb
@@ -1,3 +1,7 @@
+require_relative 'new_release_price'
+require_relative 'childrens_price'
+require_relative 'regular_price'
+
 class Movie
   attr_accessor :price_code, :title
 
@@ -8,6 +12,7 @@ class Movie
   def initialize(title, price_code)
     @title = title
     @price_code = price_code
+    @price = initialize_price
   end
 
   def charge(days_rented)
@@ -33,5 +38,18 @@ class Movie
 
   def frequent_renter_points(days_rented)
     (price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
+  end
+
+  private
+
+  def initialize_price
+    case price_code
+    when CHILDRENS
+      ChildrensPrice.new
+    when NEW_RELEASE
+      NewReleasePrice.new
+    when REGULAR
+      RegularPrice.new
+    end
   end
 end
---
+++ new_release_price.rb
@@ -0,0 +1,4 @@
+require_relative 'base_price'
+
+class NewReleasePrice < BasePrice
+end
